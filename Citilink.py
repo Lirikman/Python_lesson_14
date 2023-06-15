2a6e644e3ee9221d10ba63a0be879e11e7f6f79b
GolubevDA <999gothicman999@mail.ru>
2023-06-15 16:58:44 +0700

    parsing rabota.ru

--- Citilink.py
+++ Citilink.py
@@ -4,15 +4,26 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://www.rabota.ru/vacancy/?query=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82%20python&page=1'
+url_1 = 'https://api.hh.ru/vacancies'
+params = {
+    'text': 'NAME:(Python)',
+    'area': 1,
+    'page': 1,
+    'per_page': 20
+}
 
 page = requests.get(url)
+page_1 = requests.get(url_1, params=params)
 
-print(page.status_code)
+print(page_1.status_code)
 
 soup = BeautifulSoup(page.text, 'html.parser')
+soup_1 = BeautifulSoup(page_1.text, 'html.parser')
+print(soup_1)
 
 # Парсинг названий компаний
 company_name = soup.find_all('span', class_='vacancy-preview-card__company-name')
+company_name_1 = soup.find_all('professional_roles', class_='name')
 
 # Парсинг заголовков вакансий
 reviews = soup.find_all('h3', class_='vacancy-preview-card__title')
@@ -37,21 +48,24 @@ for i in reviews:
 for i in description:
     overflow.append(i.text)
 
-full_text = ['Company', 'Title', 'Overflow', 'Link']
+title_text = ['Company', 'Title', 'Overflow', 'Link']
 
-#for i in range(len(company)):
-#    full_text.append([company[i] + ';' + title[i] + ';' + overflow[i] + ';' + href[i]])
-#    full_text.append(overflow[i])
-#    full_text.append(href[i])
+full_text = []
+
+for i in range(len(company)):
+    full_text.append([])
+    full_text[i].append(company[i])
+    full_text[i].append(title[i])
+    full_text[i].append(overflow[i])
+    full_text[i].append(href[i])
 
 with open('vacancyes.csv', 'w') as f:
     writer = csv.writer(f, delimiter=';')
-    writer.writerow(full_text)
+    writer.writerow(title_text)
 
-with open('vacancyes.csv', 'w') as f:
+with open('vacancyes.csv', 'a') as f:
     writer = csv.writer(f, delimiter=';')
-    writer.writerow(company)
+    for i in full_text:
+        writer.writerow(i)
 
-print(full_text)
-# for rev in reviews:
-#    print(rev)
+print(company_name_1)
