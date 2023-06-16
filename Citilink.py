35c6ac6528d08cce249c821c5c7488ae5c35be0e
GolubevDA <999gothicman999@mail.ru>
2023-06-16 15:27:40 +0700

    parsing rabota.ru

--- Citilink.py
+++ Citilink.py
@@ -2,50 +2,52 @@ import re
 import csv
 from bs4 import BeautifulSoup
 import requests
+from fake_useragent import UserAgent
 
 url = 'https://www.rabota.ru/vacancy/?query=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82%20python&page=1'
-url_1 = 'https://api.hh.ru/vacancies'
-params = {
-    'text': 'NAME:(Python)',
-    'area': 1,
-    'page': 1,
-    'per_page': 20
-}
+url_1 = 'https://hh.ru/search/vacancy?text=Python&salary=&area=1&ored_clusters=true'
+
+user = UserAgent()
+headers = {'User-Agent': user.random}
 
 page = requests.get(url)
-page_1 = requests.get(url_1, params=params)
+page_1 = requests.get(url_1, headers=headers)
 
 print(page_1.status_code)
 
-soup = BeautifulSoup(page.text, 'html.parser')
-soup_1 = BeautifulSoup(page_1.text, 'html.parser')
-print(soup_1)
+soup_rabota = BeautifulSoup(page.text, 'html.parser')
+# soup_hh = BeautifulSoup(page_1.content, 'lxml')
+soup_hh = BeautifulSoup(page_1.text, 'html.parser')
+# print(soup_1)
 
 # Парсинг названий компаний
-company_name = soup.find_all('span', class_='vacancy-preview-card__company-name')
-company_name_1 = soup.find_all('professional_roles', class_='name')
+company_name_rabota = soup_rabota.find_all('span', class_='vacancy-preview-card__company-name')
+company_name_hh = soup_hh.find_all(attrs={'class': 'bloko-link bloko-link_kind-tertiary'})
 
 # Парсинг заголовков вакансий
-reviews = soup.find_all('h3', class_='vacancy-preview-card__title')
+reviews_rabota = soup_rabota.find_all('h3', class_='vacancy-preview-card__title')
+reviews_hh = soup_hh.find_all('a', class_='serp-item__title')
 
 # Парсинг описаний вакансий
-description = soup.find_all('div', class_='vacancy-preview-card__short-description')
+description_rabota = soup_rabota.find_all('div', class_='vacancy-preview-card__short-description')
+description_hh = soup_hh.find_all('a', class_='serp-item__title')
 
+# Систематизация информации по блокам: Компания, Вакансия, Описание, Ссылка
 company = []
 title = []
 overflow = []
 href = []
 
-for i in company_name:
+for i in company_name_rabota:
     company.append(i.text[17:-15])
 
-for i in reviews:
+for i in reviews_rabota:
     href.append('https://www.rabota.ru/' + i.a['href'])
 
-for i in reviews:
+for i in reviews_rabota:
     title.append(i.text[17:-15])
 
-for i in description:
+for i in description_rabota:
     overflow.append(i.text)
 
 title_text = ['Company', 'Title', 'Overflow', 'Link']
@@ -59,6 +61,8 @@ for i in range(len(company)):
     full_text[i].append(overflow[i])
     full_text[i].append(href[i])
 
+# Запись информации в файл - vacancyes.csv
+
 with open('vacancyes.csv', 'w') as f:
     writer = csv.writer(f, delimiter=';')
     writer.writerow(title_text)
@@ -68,4 +72,4 @@ with open('vacancyes.csv', 'a') as f:
     for i in full_text:
         writer.writerow(i)
 
-print(company_name_1)
+print(company_name_hh)
